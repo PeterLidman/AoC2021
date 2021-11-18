c0ef7a8c11eab23c749ba84171e8a402e7771000
Peter Lidman <peterlidman@gmail.com>
2021-11-18 21:27:15 +0100

    test

---
+++ AoC2021/src/AoC2021/L01a.java
@@ -0,0 +1,36 @@
+package AoC2021;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class L01a {
+	private static List<Integer> expenses;
+
+	public static void run(String ext) {
+		try {
+			expenses = Files.readAllLines(Paths.get("./src/AoC2020/L01input" + ext + ".txt")).stream()
+					.map(Integer::valueOf).collect(Collectors.toList());
+		} catch (IOException e) {
+			System.out.println("Knas med filimport: " + e);
+		}
+
+		int a, b;
+		boolean working = true;
+
+		for (a = 0; (a < expenses.size() - 1) && working; a++) {
+			for (b = a + 1; (b < expenses.size()) && working; b++) {
+//				System.out.println(a);
+//				System.out.println(b);
+				if (expenses.get(a) + expenses.get(b) == 2020) {
+					working = false;
+//					System.out.println(expenses.get(a));
+//					System.out.println(expenses.get(b));
+					System.out.println("Sum " + (expenses.get(a) * expenses.get(b)));
+				}
+			}
+		}
+	}
+}
---
+++ AoC2021/src/module-info.java
@@ -0,0 +1,3 @@
+module AoC2021 {
+	requires java.logging;
+}
