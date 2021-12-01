6fb14c4dfb77a3b652db7a166ed2bb52866832ff
Peter Lidman <peterlidman@gmail.com>
2021-12-01 17:30:22 +0100

    ho ho ho ho

--- AoC2021/src/AoC2021/L01b.java
+++ AoC2021/src/AoC2021/L01b.java
@@ -3,10 +3,11 @@ package AoC2021;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class L01a {
+public class L01b {
 	private static List<Integer> depth;
 
 	public static void run(String ext) {
@@ -17,11 +18,16 @@ public class L01a {
 			System.out.println("fel vid filimport: " + e);
 		}
 
-		int currentDepth = depth.get(0);
+		List<Integer> threeSum = new ArrayList<>();
+		for (int a = 0; a < depth.size() - 2; a++) {
+			threeSum.add(depth.get(a) + depth.get(a + 1) + depth.get(a + 2));
+		}
+
+		int currentDepth = threeSum.get(0);
 		int noIncreased = 0;
 		int newDepth;
-		for (int a = 1; a < depth.size(); a++) {
-			newDepth = depth.get(a);
+		for (int a = 1; a < threeSum.size(); a++) {
+			newDepth = threeSum.get(a);
 
 			if (currentDepth < newDepth) {
 				noIncreased++;
