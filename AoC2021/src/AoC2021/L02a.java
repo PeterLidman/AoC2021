73488427d83ba15baf91f336cb5d3054baf4bc7b
Peter Lidman <peterlidman@gmail.com>
2021-12-02 12:53:49 +0100

    dive dive dive

---
+++ AoC2021/src/AoC2021/L02a.java
@@ -0,0 +1,35 @@
+package AoC2021;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class L02a {
+	private static List<String> cmd;
+
+	public static void run(String ext) {
+		try {
+			cmd = Files.readAllLines(Paths.get("./src/AoC2021/L02input" + ext + ".txt")).stream().map(String::valueOf)
+					.collect(Collectors.toList());
+		} catch (IOException e) {
+			System.out.println("Fel vid filimport: " + e);
+		}
+		int depth = 0, forward = 0;
+
+		for (int a = 0; a < cmd.size(); a++) {
+//			System.out.println(cmd.get(a));
+			String c[] = cmd.get(a).split(" ");
+			if (c[0].equals("up")) {
+				depth -= Integer.valueOf(c[1]);
+			} else if (c[0].equals("down")) {
+				depth += Integer.valueOf(c[1]);
+			} else {
+				forward += Integer.valueOf(c[1]);
+			}
+		}
+		System.out.println("Product of depth and forward: " + (depth * forward));
+	}
+
+}
