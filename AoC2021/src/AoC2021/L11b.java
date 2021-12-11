e67d721038931779f22d75bc39e45d129bd4c1a0
Peter Lidman <peterlidman@gmail.com>
2021-12-11 12:51:09 +0100

    blinding flashes

---
+++ AoC2021/src/AoC2021/L11b.java
@@ -0,0 +1,125 @@
+package AoC2021;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class L11b {
+	private static List<String> d;
+	private static Integer grid[][] = new Integer[10][10];
+	private static int s, c, pc;
+
+	public static void run(String ext) throws IOException {
+		d = Files.readAllLines(Paths.get("./src/AoC2021/L11input" + ext + ".txt")).stream()
+				.collect(Collectors.toList());
+
+		s = d.get(0).length();
+		c = 0;
+		for (int x = 0; x < d.size(); x++) {
+			for (int y = 0; y < s; y++) {
+				grid[x][y] = Integer.valueOf(d.get(x).substring(y, y + 1));
+			}
+		}
+		for (int i = 0; i < 1_000_000; i++) {
+			pc = c;
+			incAll();
+			flash();
+			if (c - pc > 99) {
+				System.out.println("All flash at: " + (i + 1));
+				break;
+			}
+		}
+	}
+
+	static void show() {
+		for (int x = 0; x < d.size(); x++) {
+			for (int y = 0; y < s; y++) {
+				System.out.print(grid[x][y]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	static void incAll() {
+		for (int x = 0; x < d.size(); x++) {
+			for (int y = 0; y < s; y++) {
+				grid[x][y]++;
+			}
+		}
+	}
+
+	static void flash() {
+		for (int x = 0; x < d.size(); x++) {
+			for (int y = 0; y < s; y++) {
+				spreadFlash(x, y);
+			}
+		}
+		for (int x = 0; x < d.size(); x++) {
+			for (int y = 0; y < s; y++) {
+				if (grid[x][y] < 0) {
+					grid[x][y] = 0;
+				}
+			}
+		}
+	}
+
+	private static void spreadFlash(int x, int y) {
+		if (grid[x][y] > 9) {
+			c++;
+			grid[x][y] = -1000;
+			if (x > 0) {
+				grid[x - 1][y]++;
+				if (y > 0) {
+					grid[x - 1][y - 1]++;
+				}
+				if (y < 9) {
+					grid[x - 1][y + 1]++;
+				}
+			}
+			if (y > 0) {
+				grid[x][y - 1]++;
+			}
+			if (x < 9) {
+				grid[x + 1][y]++;
+				if (y > 0) {
+					grid[x + 1][y - 1]++;
+				}
+				if (y < 9) {
+					grid[x + 1][y + 1]++;
+				}
+			}
+			if (y < 9) {
+				grid[x][y + 1]++;
+			}
+
+			if (x > 0) {
+				spreadFlash(x - 1, y);
+				if (y > 0) {
+					spreadFlash(x - 1, y - 1);
+				}
+				if (y < 9) {
+					spreadFlash(x - 1, y + 1);
+				}
+			}
+			if (y > 0) {
+				spreadFlash(x, y - 1);
+			}
+			if (x < 9) {
+				spreadFlash(x + 1, y);
+				if (y > 0) {
+					spreadFlash(x + 1, y - 1);
+				}
+				if (y < 9) {
+					spreadFlash(x + 1, y + 1);
+				}
+			}
+			if (y < 9) {
+				spreadFlash(x, y + 1);
+			}
+		}
+	}
+
+}
