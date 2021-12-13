16786ffeb80005d8d8262e2eeb672c816ea96772
Peter Lidman <peterlidman@gmail.com>
2021-12-13 15:39:26 +0100

    record och pattern

---
+++ AoC2021/src/AoC2021/L13b.java
@@ -0,0 +1,96 @@
+package AoC2021;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class L13b {
+	private static List<String> dots;
+
+	record dot(int x, int y) {
+	}
+
+	record fold(char direction, int position) {
+	}
+
+	static List<dot> ld;
+	static List<fold> lf;
+	static Pattern pd = Pattern.compile("(\\d+),(\\d+)");
+	static Pattern pf = Pattern.compile("fold along ([xy])=(\\d+)");
+
+	public static void run(String ext) throws IOException {
+		dots = Files.readAllLines(Paths.get("./src/AoC2021/L13input" + ext + ".txt")).stream()
+				.collect(Collectors.toList());
+
+		ld = new ArrayList<>();
+		lf = new ArrayList<>();
+		for (String a : dots) {
+			if (a.length() > 0) {
+				if (a.charAt(0) == 'f') {
+					Matcher m = pf.matcher(a);
+					m.find();
+					lf.add(new fold(m.group(1).charAt(0), Integer.valueOf(m.group(2))));
+				} else {
+					Matcher m = pd.matcher(a);
+					m.find();
+					ld.add(new dot(Integer.valueOf(m.group(1)), Integer.valueOf(m.group(2))));
+				}
+			}
+		}
+		for (int i = 0; i < lf.size(); i++) {
+			foldAt(lf.get(i).direction, lf.get(i).position);
+		}
+//		System.out.println(ld.size() + " dots are visible");
+		show();
+	}
+
+	static void foldAt(char d, int at) {
+		List<dot> tdots = new ArrayList<>(ld);
+		if (d == 'y') {
+			for (int i = 0; i < tdots.size(); i++) {
+				if (tdots.get(i).y > at) { // flytta upp
+					dot td = new dot(tdots.get(i).x, 2 * at - tdots.get(i).y);
+					if (!ld.contains(td)) {
+						ld.add(td);
+					}
+					ld.remove(new dot(tdots.get(i).x, tdots.get(i).y));
+				}
+			}
+		} else {
+			for (int i = 0; i < tdots.size(); i++) {
+				if (tdots.get(i).x > at) { // flytta in
+					dot td = new dot(2 * at - tdots.get(i).x, tdots.get(i).y);
+					if (!ld.contains(td)) {
+						ld.add(td);
+					}
+					ld.remove(new dot(tdots.get(i).x, tdots.get(i).y));
+				}
+			}
+		}
+	}
+
+	static void show() {
+		int maxx = 0, maxy = 0;
+		for (int i = 0; i < ld.size(); i++) {
+			if (ld.get(i).x > maxx) {
+				maxx = ld.get(i).x;
+			}
+			if (ld.get(i).y > maxy) {
+				maxy = ld.get(i).y;
+			}
+		}
+		for (int y = 0; y <= maxy; y++) {
+			for (int x = 0; x <= maxx; x++) {
+				System.out.print(ld.contains(new dot(x, y)) ? "#" : ".");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+}
